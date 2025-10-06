7634cb790e2e075da5edcc1d515bd6d29b08841f
PB <piers.bryn1@outlook.com>
2025-10-06 20:46:31 +0100

    tried knife edge fit which failed

--- Optics and Laser Beams/knife_edge_fitter.py
+++ Optics and Laser Beams/knife_edge_fitter.py
@@ -20,25 +20,27 @@ def gaussian(x, x0, w):
 x = "distance (mm)"
 y = "voltage (V)"
 
-for trace in ["knife_edge_data"]:
-    df = pd.read_csv(trace + ".csv")
-    plt.scatter(df[x], df[y])
-    popt, pcov = curve_fit(beam, df[x], df[y], p0=(5, 1, 15, 0))
-    perr = np.sqrt(np.diag(pcov))
-    xfit = np.linspace(min(df[x]), max(df[x]), 1000)
-    plt.plot(xfit, beam(xfit, *popt), c="C1")
-    plt.xlabel(x)
-    plt.ylabel(y)
-    plt.title(trace)
-    plt.show()
+file = r'C:\Users\pears\Documents\Piers Files\Work & School\PhD QLM (OLD NOW USING ONEDRIVE)\Local Files Only\QLM-Experimental-Skills\Optics and Laser Beams\knife_edge_data.csv'
+header = "knife edge fit"
 
-    print("\n== {} ==".format(trace))
-    for arg, val, err in zip(beam.__code__.co_varnames[1:], popt, perr):
-        prec = floor(log10(err))
-        err = round(err / 10**prec) * 10**prec
-        val = round(val / 10**prec) * 10**prec
-        if prec > 0:
-            valerr = "{:.0f}({:.0f})".format(val, err)
-        else:
-            valerr = "{:.{prec}f}({:.0f})".format(val, err * 10**-prec, prec=-prec)
-        print(arg, "=", valerr)
+df = pd.read_csv(file)
+plt.scatter(df[x], df[y])
+popt, pcov = curve_fit(beam, df[x], df[y], p0=(5, 1, 15, 0))
+perr = np.sqrt(np.diag(pcov))
+xfit = np.linspace(min(df[x]), max(df[x]), 1000)
+plt.plot(xfit, beam(xfit, *popt), c="C1")
+plt.xlabel(x)
+plt.ylabel(y)
+plt.title(header)
+plt.show()
+
+print("\n== {} ==".format(header))
+for arg, val, err in zip(beam.__code__.co_varnames[1:], popt, perr):
+    prec = floor(log10(err))
+    err = round(err / 10**prec) * 10**prec
+    val = round(val / 10**prec) * 10**prec
+    if prec > 0:
+        valerr = "{:.0f}({:.0f})".format(val, err)
+    else:
+        valerr = "{:.{prec}f}({:.0f})".format(val, err * 10**-prec, prec=-prec)
+    print(arg, "=", valerr)
