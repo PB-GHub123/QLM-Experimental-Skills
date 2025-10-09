985c513f4c14eecf5c97b058c40ab466a12688b3
PBryn <piers.bryn1@outlook.com>
2025-10-09 16:06:41 +0100

    more captures

--- Rb Spectroscopy/personal/test_plot.py
+++ Rb Spectroscopy/personal/test_plot.py
@@ -1,9 +1,9 @@
 import numpy as np
 from matplotlib import pyplot as plt
 
-spectrum = np.loadtxt('capture_2_wider.csv',delimiter = ',',dtype='float', skiprows=2)
+spectrum = np.loadtxt('capture_3_michael.csv',delimiter = ',',dtype='float', skiprows=2)
 spectrum.transpose()
 
 plt.figure(1)
-plt.plot(spectrum[0], spectrum[2])
+plt.plot(np.arange(spectrum.shape[0]), spectrum[:,1])
 plt.show()
