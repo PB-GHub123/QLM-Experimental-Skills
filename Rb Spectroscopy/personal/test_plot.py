d0af16479ac979c6156cc186a34dde4fa85ac279
PBryn <piers.bryn1@outlook.com>
2025-11-10 11:12:29 +0000

    syncing, issue with time axis

--- Rb Spectroscopy/personal/test_plot.py
+++ Rb Spectroscopy/personal/test_plot.py
@@ -2,11 +2,11 @@ import numpy as np
 from matplotlib import pyplot as plt
 import pandas as pd
 
-spectrum = pd.read_csv("./Rb Spectroscopy/personal/sub doppler averaging.csv")
+"""spectrum = pd.read_csv("./Rb Spectroscopy/personal/sub doppler 1.csv")
 spectrum = spectrum.values[1:].astype(float)
 
 plt.figure(1)
 plt.scatter(spectrum[:,0], spectrum[:,1], s=5)
 plt.figure(2)
 plt.scatter(spectrum[:,0], spectrum[:,2], s=5)
-plt.show()
+plt.show()"""
